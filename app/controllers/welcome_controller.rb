30117207eb37a14523fb68c06b9029c50ea74ba5
kjordan88 <kristy.jordan88@gmail.com>
2017-11-11 20:08:39 -0500

    builds preliminary welcome page and controller

---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,6 @@
+class WelcomeController < ApplicationController
+
+  def index
+    # @categories = Categories.all
+  end
+end
---
+++ app/models/category.rb
@@ -0,0 +1,3 @@
+class Category < ActiveRecord::Base
+  attr_accessor :name
+end
